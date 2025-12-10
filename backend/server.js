8cc2a97a04a5af4fadf1a0678853e5bef5ddb7c6
steel77-7 <utkarshkaundal1970@gmail.com>
2025-12-10 23:30:02 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,39 @@
-import express from "express"
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from "./config/mongodb.js"
-import connectCloudinary from "./config/cloudinary.js"
-import userRouter from "./routes/userRoute.js"
-import doctorRouter from "./routes/doctorRoute.js"
-import adminRouter from "./routes/adminRoute.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
-const app = express()
+const app = express();
 
 var corspermission = {
-  origin: '*',
+  origin: process.env.FRONTEND_URL,
   methods: "POST,GET,DELETE,PATCH,PUT,HEAD",
-  credentials: true
-}
-app.use(cors(corspermission))
+  credentials: true,
+};
+app.use(cors(corspermission));
 
-const port = process.env.PORT || 4000
-connectDB()
-connectCloudinary()
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/admin", adminRouter)
-app.use("/api/doctor", doctorRouter)
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working")
+  res.send("API Working");
 });
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`))
-
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
 
 // bnfn sfnw elcv cusx
